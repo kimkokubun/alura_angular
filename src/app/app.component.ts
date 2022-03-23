6b49db377a9902889127d1c4850f86b17b68ee8b
Kim Kokubun <kim.kokubun@gmail.com>
2022-03-22 21:04:35 -0300

    add event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {$e} from "@angular/compiler/src/chars";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir(event: any) {
+    this.transferencia = event.event;
+    console.log(this.transferencia)
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaTransferenciaComponent implements OnInit {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor?: number;
+  destino?: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  transferir(){
+    console.log('Solicitado nova transferencia');
+    const valorEmitir = {valor :this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
+
